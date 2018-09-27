f5d63c75efe0780799d20f3a1ef44d559bca4699
nadavk123 <nadavschool1@gmail.com>
2018-09-27 18:58:38 +0300

    better version of main

--- src/gits.py
+++ src/gits.py
@@ -6,6 +6,9 @@ import file_core
 import os
 import glob
 
+BAD_CHARACTERS = r"-/\!@#$%^&*(){}[]`~';|-=_+,.?><" + '"'
+
+
 def get_cur_branch():
     """
     Get the current branch
@@ -13,6 +16,11 @@ def get_cur_branch():
     """
     with open("./gits/HEAD.txt", "rb") as f:
         return f.read()
+
+
+def get_branches():
+    branches = [os.path.basename(path) for path in glob.glob("gits/branches/*")]
+    return branches
 
 
 def move_branch(to):
@@ -39,20 +47,9 @@ def get_cur_commit(name):
         return log[-1].split("|")[0]
 
 
-def init():
-    """
-    Initials the gits folder in master branch
-    """
-    if is_repo():
-        print "Alredy a Gits repository"
-        return
-
-    os.mkdir("gits")
-    os.mkdir("gits/branches")
-    os.mkdir("gits/refs")
-    os.mkdir("gits/index")
-    with open("gits/HEAD.txt", "wb") as f:
-        f.write("master")
+def get_branch_commit():
+    cur_branch = get_cur_branch()
+    return cur_branch, get_cur_commit(cur_branch)
 
 
 def is_repo():
@@ -75,56 +72,104 @@ def add_file(filename):
         f.write("\r\n{}".format(filename))
 
 
-def main():
-    if len(sys.argv) == 1:
-        print "Gits by Nadav"
-        exit()
+def handle_init(param):
+    """
+    Initials the gits folder in master branch
+    """
+    if is_repo():
+        print "\nAlredy a Gits repository"
+        return
 
-    if sys.argv[1] == "init":
-        init()
-        print "Done"
+    dirs_to_create = ["gits", "gits/branches", "gits/refs", "gits/index"]
 
-    if not os.path.isdir("gits"):
-        print "Not a Gits repository"
-        exit()
+    for dir in dirs_to_create:
+        os.mkdir(dir)
+    with open("gits/HEAD.txt", "wb") as f:
+        f.write("master")
+    print "\nRepository created successfully."
 
-    cur_branch = get_cur_branch()
-    cur_commit = get_cur_commit(cur_branch)
 
-    if sys.argv[1] == "status":
-        core_functions.status(cur_branch, cur_commit)
+def handle_status(param):
+    core_functions.status(*get_branch_commit())
 
-    if sys.argv[1] == "add":
-        add_file(sys.argv[2])
 
-    if sys.argv[1] == "commit":
-        core_functions.commit(cur_branch, sys.argv[2])
-
-    if sys.argv[1] == "log":
-        log = branch.get_log(cur_branch)
-        print log
-        if log == ['']:
-            print "There are no commits yes!"
-            exit()
-        print "\r\nLog of branch {}\r\n".format(cur_branch)
-        for line in log:
-            print "\t" + "  ".join(line.split("|")[::-1])
-        exit()
-
-    if sys.argv[1] == "checkout":
-        if len(sys.argv) == 3:
-            move_branch(sys.argv[2])
-        elif len(sys.argv) == 4 and sys.argv[2] == "-b":
-            branch.create_new_branch(cur_branch, sys.argv[3])
-            move_branch(sys.argv[3])
+def handle_add(param):
+    for filename in param:
+        if os.path.exists(filename):
+            add_file(filename)
         else:
-            raise Exception()
+            print "file: {} does not exists"
 
-    if sys.argv[1] == "branch":
-        branches = glob.glob("gits/branches/*")
-        print "Branches:"
-        for i in branches:
-            print "\t" + os.path.basename(i)
+
+def handle_commit(param):
+    core_functions.commit(get_cur_branch(), param[0])
+
+
+def handle_log(param):
+    cur_branch = get_cur_branch()
+    log = branch.get_log(cur_branch)
+
+    print log
+    if log == ['']:
+        print "There are no commits yes!"
+        exit()
+    print "\r\nLog of branch {}\r\n".format(cur_branch)
+    for line in log:
+        print "\t" + "  ".join(line.split("|")[::-1])
+    exit()
+
+
+def handle_checkout(param):
+    if len(param) > 2 or len(param) == 0:
+        print "Wrong number of parameters.\n"
+
+    repo_branches = get_branches()
+    if len(param) == 1:
+        if param[0] in repo_branches:
+            move_branch(param[0])
+        else:
+            print "{} is not a Branch.".format(param[0])
+            print "Use -b to create new branch.\n"
+
+    if len(param) == 2 and param[0] == "-b":
+        branch.create_new_branch(get_cur_branch(), param[0])
+        move_branch(param[0])
+
+
+
+
+def handle_branch(param):
+    print "\nBranches: \n"
+    for branch in get_branches():
+        print "\t{}".format(branch)
+
+
+def handle_input():
+    parameters = sys.argv[1:]
+    handle_dict = {"init": handle_init, "status": handle_status, "add": handle_add,
+                   "commit": handle_commit, "log": handle_log, "checkout": handle_checkout,
+                   "branch": handle_branch}
+    if len(parameters) == 0:
+        print "\nGits by Nadav\n"
+        print "Available Commands:\r\n"
+        for i in handle_dict.keys():
+            print "\t{}".format(i)
+
+    elif parameters[0] == "init":
+        handle_init(parameters)
+
+    elif not is_repo() and parameters[0] != "init":
+        print "Not a Gits repository"
+    else:
+        cur_branch = get_cur_branch()
+        cur_commit = get_cur_commit(cur_branch)
+
+        handle_dict[parameters[0]](parameters[1:])
+
+
+def main():
+    handle_input()
+
 
 if __name__ == '__main__':
     main()
